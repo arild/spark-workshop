7ef695a40f5e368a2879476fdf1b978906568c6f
Henrik Nordvik <henrikno@gmail.com>
2015-05-14 18:01:52 +0200

    Rename test

--- src/test/scala/workshop/util/IpLookupTest.scala
+++ src/test/scala/workshop/util/IpLookupTest.scala
@@ -3,7 +3,7 @@ package workshop.util
 import org.scalatest.Matchers
 
 class IpLookupTest extends SparkTestUtils with Matchers {
-  test("test") {
+  test("should get country from ip in US") {
     IpLookup.getCountryForIp("1.2.3.4").get should be("United States")
 
   }
