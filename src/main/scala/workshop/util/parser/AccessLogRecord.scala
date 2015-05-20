cf9c55d9d2c7073c29c36f9a56c9660f5ff23e87
Arild Nilsen <arild.nilsen2@gmail.com>
2015-05-20 15:10:04 +0200

    Add slide describing tables for part2

--- src/main/scala/workshop/util/parser/AccessLogRecord.scala
+++ src/main/scala/workshop/util/parser/AccessLogRecord.scala
@@ -1,16 +1,13 @@
 package workshop.util.parser
 
-/**
- * @see http://httpd.apache.org/docs/2.2/logs.html for details
- */
 case class AccessLogRecord (
-    ipAddress: String,         // should be an ip address, but may also be the hostname if hostname-lookups are enabled
-    rfc1413ClientIdentity: String,   // typically `-`
-    remoteUser: String,              // typically `-`
-    dateTime: String,                // [day/month/year:hour:minute:second zone]
-    request: String,                 // `GET /foo ...`
-    status: Int,                     // 200, 404, etc.
-    bytesSent: Int                // may be `-`
+    ipAddress: String,
+    rfc1413ClientIdentity: String,  // typically `-`
+    remoteUser: String,             // typically `-`
+    dateTime: String,               // [day/month/year:hour:minute:second zone]
+    request: String,                // `GET /foo ...`
+    status: Int,                    // 200, 404, etc.
+    bytesSent: Int
 )
 
 
--- src/main/scala/workshop/util/parser/HttpStatusCode.scala
+++ src/main/scala/workshop/util/parser/HttpStatusCode.scala
@@ -1,3 +1,7 @@
 package workshop.util.parser
 
-case class HttpStatusCode(status: Int, description: String, status_type: String)
+case class HttpStatusCode(
+    status: Int,         // 200, 404, etc.
+    description: String, // "OK", "Not Found", etc.
+    status_type: String  // "Successful", "Client Error", etc.
+)
